3a03773e131bf513e050b57be92ee5ef42b86a3e
Hannah Y <hannah@corgibyte.com>
2021-10-18 10:36:46 -0700

    Initial calculator functions.
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    co-authored-by: Nick Benzinger <nicholasbenzinger@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtraction(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number1, number2));
